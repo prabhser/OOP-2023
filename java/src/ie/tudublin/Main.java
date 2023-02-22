91348a2053f9f9641671e0d6570ab2c90268ca01
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-02-22 14:04:15 +0000

    grid2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -30,14 +30,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		// System.out.println("Hello world");
-		
-		// Dog penny = new Dog();
-		// penny.setName("Penny");
-		// penny.speak();
-
 		grid();
-		
 	}
 	
 }
