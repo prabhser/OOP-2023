2e00326ac1751cd84b7ea803ed5a363094c94c2f
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-03-06 12:22:52 +0000

    partdone

--- java/src/ie/tudublin/LifeBoard.java
+++ java/src/ie/tudublin/LifeBoard.java
@@ -7,12 +7,13 @@ public class LifeBoard {
     boolean[][] board;
     private int size;
     PApplet p;
+    float cellWidth;
 
     public LifeBoard(int size, PApplet p){
         this.size = size;
-        this.p = p;
         board = new boolean[size][size];
-        float cellWidth;
+        this.p = p;
+        cellWidth = p.width / (float) size;
     }
 
     public void randomise(){
@@ -27,7 +28,6 @@ public class LifeBoard {
     public void render(){
         for (int r = 0; r < size; r++) {
             for (int c = 0; c < board.length; c++) { // randomises board so that some cells dead, alive
-                cellWidth = p.
                 float x = c * cellWidth;
                 float y = r * cellWidth;
                 if (board[r][c] == true)
@@ -35,17 +35,18 @@ public class LifeBoard {
                 else
                     p.noFill();
 
+                p.rect(x, y, cellWidth, cellWidth);
                     //p.square(c*size, r*size, 1);;
             }
         }
     }
 
-    public getSize(){  
-        return size;
-    }
+    // public getSize(){  
+    //     return size;
+    // }
 
-    public setSize(int size){
+    // public setSize(int size){
 
-    }
+    // }
 
 }
