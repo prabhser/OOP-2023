9fac8500b46bcf3c8ce4c16f181cf41448df9593
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-03-20 12:09:42 +0000

    errorless

--- java/src/ie/tudublin/Ship.java
+++ java/src/ie/tudublin/Ship.java
@@ -1,23 +1,54 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Ship {
-    
-    PApplet p;
+    private PVector pos;
+    private PApplet p;
 
-    public ship
+    public Ship(float x, float y, float size, int c, PApplet p)
+    {
+        pos = new PVector(x, y);
+        this.size = size;
+        this.c = c;       
+        this.p = p;  
+    }
 
 
+
+    public PVector getPos() {
+        return pos;
+    }
+    public void setPos(PVector pos) {
+        this.pos = pos;
+    }
+    public float getRot() {
+        return rot;
+    }
+    public void setRot(float rot) {
+        this.rot = rot;
+    }
+    public int getC() {
+        return c;
+    }
+    public void setC(int c) {
+        this.c = c;
+    }
+    public float getSize() {
+        return size;
+    }
+    public void setSize(float size) {
+        this.size = size;
+    }
     private float rot;
     private int c;
     private float size;
 
-    public void render(){
-        p.stroke(c, 255, 255);
-        p.line(pos.x-halfSize, pos.y+halfSize, pos.x, pos.y-halfSize);
-        p.line(pos.x, pos.y-halfSize, pos.x+halfSize, pos.y+halfSize);
-        p.line
-
+    public void render()
+    {
     }
+
+    
+    
 }
